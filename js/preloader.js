009351b792157d0449736dffa795ecf14a1661f6
Shovik Poddar <shovikmetal@gmail.com>
2021-08-24 00:46:01 +0530

    edit-1

--- js/preloader.js
+++ js/preloader.js
@@ -60,7 +60,7 @@ var strings = [
   "Loading content",
   "Page rendered",
   "Starting display manager",
-  "WELCOME TO LAKSHYA CTF INC 2020",
+  "WELCOME TO eHAcon CTF 2021",
   "Initializing..."
 ];
 
